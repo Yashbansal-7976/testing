0dbf025785974d351fe4e59aed42b4c64dd1da68
Yash Bansal <114819833+Yashbansal-7976@users.noreply.github.com>
2026-04-22 16:22:05 +0530

    Remove insert function and add sum_numbers function

--- rohit.py
+++ rohit.py
@@ -1,17 +1,5 @@
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    
-    current = root
-    while True:
-        if key < current.val:
-            if current.left is None:
-                current.left = Node(key)
-                break
-            current = current.left
-        else:
-            if current.right is None:
-                current.right = Node(key)
-                break
-            current = current.right
-    return root   
+def sum_numbers(numbers):
+    result = 0
+    for value in numbers:
+        result += value
+    return result
