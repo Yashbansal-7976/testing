df04d71157c1103a6fad27741097e181632ae804
Yash Bansal <114819833+Yashbansal-7976@users.noreply.github.com>
2026-04-22 16:03:54 +0530

    Add tree traversal methods and node class

--- yash.py
+++ yash.py
@@ -1,9 +1,53 @@
-x=int(input("Enter the number: "))
-a=0
-b=1
-print(a, b, end=' ')
-for i in range(x-2):
-    c=a+b
-    a=b
-    b=c
-    print(b, end=' ')
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def in_order_dfs(node):
+    if node is None:
+        return
+    in_order_dfs(node.left)
+    print(node.data, end=' ')
+    in_order_dfs(node.right)
+
+def pre_order_dfs(node):
+    if node is None:
+        return
+    print(node.data, end=' ')
+    pre_order_dfs(node.left)
+    pre_order_dfs(node.right)
+
+def post_order_dfs(node):
+    if node is None:
+        return
+    post_order_dfs(node.left)
+    post_order_dfs(node.right)
+    print(node.data, end=' ')
+
+def bfs(root):
+    if root is None:
+        return
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        print(node.data, end=' ')
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+if __name__ == "__main__":
+    root = Node(2)
+    root.left = Node(3)
+    root.right = Node(4)
+    root.left.left = Node(5)
+
+    print("In-order DFS: ", end='')
+    in_order_dfs(root)
+    print("\nPre-order DFS: ", end='')
+    pre_order_dfs(root)
+    print("\nPost-order DFS: ", end='')
+    post_order_dfs(root)
+    print("\nLevel order: ", end='')
+    bfs(root)
